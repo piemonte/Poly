bc44b02f29da64786449b630d716ed6a20202752
patrick piemonte <patrick.piemonte@gmail.com>
2020-06-09 22:16:49 -0700

    add support for SPM

---
+++ Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version:5.2
+//  MIT License
+//  Copyright (c) 2018-present patrick piemonte (http://patrickpiemonte.com)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+import PackageDescription
+
+let package = Package(
+    name: "Poly",
+    platforms: [
+      .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "Poly",
+            targets: ["Poly"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.0"),
+        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.13.1"),
+        .package(url: "https://github.com/hyperoslo/Cache", from: "5.3.0"),
+        .package(url: "https://github.com/saoudrizwan/Disk", from: "0.6.0"),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper", from: "4.2.0"),
+    ],
+    targets: [
+        .target(
+            name: "Poly",
+            path: "Sources",
+            dependencies: []),
+    ],
+    swiftLanguageVersions: [.v5]
+)
